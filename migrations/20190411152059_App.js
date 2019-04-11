44e0ce163802cde6a69eea298e82ac691a003db8
SharleneRyan <sa.ryan@live.com>
2019-04-11 16:03:31 +1200

    change migration dropTable

--- migrations/20190411152059_App.js
+++ migrations/20190411152059_App.js
@@ -15,6 +15,6 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return knex.schema.table('Community',function(table){
-    table.dropColumn('name');
+    table.dropTable('Community');
   })
 };
